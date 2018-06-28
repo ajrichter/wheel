c1be6eab0cc1991a3dc1ae1e3fb2ab3369ae34b7
ajrichter <26774784+ajrichter@users.noreply.github.com>
2018-06-28 13:05:51 -0500

    Update mergesort.java

--- mergesort.java
+++ mergesort.java
@@ -1,1 +1,29 @@
-// Initial
+// Writing an Implementation of Mergesort
+// Copying psuedocode from https://en.wikipedia.org/wiki/Merge_sort
+
+import java.awt.Rectangle;
+
+public class MergeSort
+{	
+  public static void main(String[] args)
+	{	go();
+	}
+	
+	public static void go()
+	{	
+	}
+	
+	public static void alterPointee(Rectangle r)
+	{	System.out.println("In method alterPointee. r " + r + "\n");
+		r.setSize(20, 30);
+		System.out.println("In method alterPointee. r " + r + "\n");
+	}
+	
+	public static void alterPointer(Rectangle r)
+	{	System.out.println("In method alterPointer. r " + r + "\n");
+		r = new Rectangle(5, 10, 30, 35);
+		System.out.println("In method alterPointer. r " + r + "\n");
+	}
+	
+	
+}
